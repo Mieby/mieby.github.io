bb41aa4a43b6a2a3bc305da273a79f355271ffbc
Mieby <emmashon2019@gmail.com>
2024-11-15 00:05:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -53,13 +53,9 @@ function pullGacha() {
             ownedPets.push(prize);
             updateOwnedPets();
         }
-    } else if (randomNum < 60.5) {  // 60% de probabilidad de obtener comida
-        prize = items.comida[Math.floor(Math.random() * items.comida.length)];
-    } else if (randomNum < 80.5) {  // 20% de probabilidad de obtener un libro
-        prize = items.libros[Math.floor(Math.random() * items.libros.length)];
-    } else {  // 10% de probabilidad de obtener un fondo
-        prize = items.fondos[Math.floor(Math.random() * items.fondos.length)];
-    }
+    } else {  // Para comida, libros y fondos
+        prize = items.comida.concat(items.libros, items.fondos)[Math.floor(Math.random() * 
+        items.comida.concat(items.libros, items.fondos).length)];
 
     // Almacenar items obtenidos en el inventario
     if (prize.type !== 'Pet') {
