dbce914cf9402860baf6fcf3e3bf49c3996257f7
Mieby <emmashon2019@gmail.com>
2024-12-07 23:20:30 -0300

    Update genshin.js

--- genshin.js
+++ genshin.js
@@ -1,9 +1,3 @@
-<script src="genshinCharacters.js"></script>
-<script>
-// Ahora puedes usar la variable genshinCharacters directamente
-console.log(genshinCharacters);
-</script>
-
 const weaponsList = [
     { name: "Espada de Favonius", weaponType: "sword", stars: 4, img: "assets/weapons/favonius_sword.png"},
     { name: "Espada de Favonius", weaponType: "sword", stars: 4, img: "assets/weapons/favonius_sword.png"},
