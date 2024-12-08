77883473770cce9800c50108f1e6606362f0d8cd
Mieby <emmashon2019@gmail.com>
2024-12-07 23:25:13 -0300

    Update genshin.js

--- genshin.js
+++ genshin.js
@@ -1,11 +1,3 @@
-const weaponsList = [
-    { name: "Espada de Favonius", weaponType: "sword", stars: 4, img: "assets/weapons/favonius_sword.png"},
-    { name: "Espada de Favonius", weaponType: "sword", stars: 4, img: "assets/weapons/favonius_sword.png"},
-    { name: "Espada de Favonius", weaponType: "sword", stars: 4, img: "assets/weapons/favonius_sword.png" },
-    { name: "Espada de Favonius", weaponType: "sword", stars: 4, img: "assets/weapons/favonius_sword.png" },
-    { name: "Espada de Favonius", weaponType: "sword", stars: 4, img: "assets/weapons/favonius_sword.png"}
-];
-
 // Elementos del DOM
 const characterGrid = document.getElementById("character-grid");
 const characterModal = document.getElementById("character-modal");
