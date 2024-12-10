f6f35a252309b341f6686592f943310ab7c0add6
Mieby <emmashon2019@gmail.com>
2024-12-10 00:07:11 -0300

    Update genshinWeapons.js

--- genshinWeapons.js
+++ genshinWeapons.js
@@ -1,10 +1,10 @@
 const weaponsList = [
 
 // espadas 4 estrellas
-    { name: "Anemona Gemela", weaponType: "sword", stars: 4, img: "https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/weapons/redhorn_stonethresher.png?strip=all&quality=100&w=236"},
-    { name: "Espada de Favonius", weaponType: "Espada", stars: 4, img: "assets/weapons/favonius_sword.png"},
-    { name: "Espada de Favonius", weaponType: "Espada", stars: 4, img: "assets/weapons/favonius_sword.png" },
-    { name: "Espada de Favonius", weaponType: "Espada", stars: 5, img: "assets/weapons/favonius_sword.png" },
-    { name: "Espada de Favonius", weaponType: "Mandoble", stars: 4, img: "assets/weapons/favonius_sword.png"}
+    { name: "Anemona Gemela", type: "sword", stars: 4, img: "https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/weapons/redhorn_stonethresher.png?strip=all&quality=100&w=236"},
+    { name: "Espada de Favonius", type: "Espada", stars: 4, img: "assets/weapons/favonius_sword.png"},
+    { name: "Espada de Favonius", type: "Espada", stars: 4, img: "assets/weapons/favonius_sword.png" },
+    { name: "Espada de Favonius", type: "Espada", stars: 5, img: "assets/weapons/favonius_sword.png" },
+    { name: "Espada de Favonius", type: "Mandoble", stars: 4, img: "assets/weapons/favonius_sword.png"}
 ];
 
