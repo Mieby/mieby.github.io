518400f90c68388d001d8c020415a8f5b873957a
Mieby <emmashon2019@gmail.com>
2024-12-10 14:21:04 -0300

    Update genshinWeapons.js

--- genshinWeapons.js
+++ genshinWeapons.js
@@ -1,10 +1,10 @@
 const weaponsList = [
 
 // espadas 4 estrellas
-    { name: "Anemona Gemela", type: "Espada", stars: 4, img: "assets/weapons/Amenoma Gemela.png"},
-    { name: "Espada del peñasco oscuro", type: "Espada", stars: 4, img: "assets/weapons/Espada del peñasco oscuro.png"},
-    { name: "Espada de Favonius", type: "Arco", stars: 5, img: "assets/weapons/favonius_sword.png" },
-    { name: "Espada de Favonius", type: "Arco", stars: 5, img: "assets/weapons/favonius_sword.png" },
-    { name: "Espada de Favonius", type: "Mandoble", stars: 5, img: "assets/weapons/favonius_sword.png"}
+    { name: "Anemona Gemela", type: "sword", stars: 4, img: "assets/weapons/Amenoma Gemela.png"},
+    { name: "Espada del peñasco oscuro", type: "sword", stars: 4, img: "assets/weapons/Espada del peñasco oscuro.png"},
+    { name: "Espada de Favonius", type: "bow", stars: 5, img: "assets/weapons/favonius_sword.png" },
+    { name: "Espada de Favonius", type: "catalist", stars: 5, img: "assets/weapons/favonius_sword.png" },
+    { name: "Espada de Favonius", type: "claymore", stars: 5, img: "assets/weapons/favonius_sword.png"}
 ];
 
