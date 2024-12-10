4390bbfc39b163ad5cd112524b8073fc731b5d11
Mieby <emmashon2019@gmail.com>
2024-12-10 16:50:00 -0300

    Update genshinCharacters.js

--- genshinCharacters.js
+++ genshinCharacters.js
@@ -12,12 +12,7 @@ const genshinCharacters = [
         artifacts: [
             { name: "Artefacto 1", img: "ruta/a/imagen/artefacto1.png" },
             { name: "Artefacto 2", img: "ruta/a/imagen/artefacto2.png" }
-        ],
-     stats: { // Nuevo campo para los stats
-        reloj: "atk",
-        caliz: "atk",
-        corona: "atk"
-    }
+        ]
     },
     { name: "Lynette", element: "Anemo", stars: 4, weaponType: "sword", img: "assets/characters/Lynette.png", 
         items: [
@@ -30,12 +25,7 @@ const genshinCharacters = [
         artifacts: [
             { name: "Artefacto 1", img: "ruta/a/imagen/artefacto1.png" },
             { name: "Artefacto 2", img: "ruta/a/imagen/artefacto2.png" }
-        ],
-     stats: { // Nuevo campo para los stats
-        reloj: "atk",
-        caliz: "atk",
-        corona: "atk"
-    }
+        ]
     },
     { name: "Faruzán", element: "Anemo", stars: 4, weaponType: "catalyst", img: "assets/characters/Faruzan-new.png", 
         items: [
@@ -46,12 +36,7 @@ const genshinCharacters = [
         ],
         artifacts: [
             { name: "Artefacto 1", img: "ruta/a/imagen/artefacto1.png" }
-        ],
-     stats: { // Nuevo campo para los stats
-        reloj: "atk",
-        caliz: "atk",
-        corona: "atk"
-    }
+        ]
     },
     { name: "Heizou", element: "Anemo", stars: 4, weaponType: "catalyst", img: "assets/characters/Heizou-new.png", 
         items: [
@@ -62,12 +47,7 @@ const genshinCharacters = [
         ],
         artifacts: [
             { name: "Artefacto 1", img: "ruta/a/imagen/artefacto1.png" }
-        ],
-     stats: { // Nuevo campo para los stats
-        reloj: "atk",
-        caliz: "atk",
-        corona: "atk"
-    }
+        ]
     },
     { name: "Sacarosa", element: "Anemo", stars: 4, weaponType: "catalyst", img: "assets/characters/Sacarosa.png", 
         items: [
@@ -78,12 +58,6 @@ const genshinCharacters = [
         ],
         artifacts: [
             { name: "Artefacto 1", img: "ruta/a/imagen/artefacto1.png" }
-        ],
-     stats: { // Nuevo campo para los stats
-        reloj: "atk",
-        caliz: "atk",
-        corona: "atk"
-    }
+        ]
     }
 ];
-
