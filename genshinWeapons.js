4da10ca89d672e11205ac1b73cb48113f815ddcd
Mieby <emmashon2019@gmail.com>
2024-12-09 22:32:47 -0300

    Update genshinWeapons.js

--- genshinWeapons.js
+++ genshinWeapons.js
@@ -1,5 +1,7 @@
 const weaponsList = [
-    { name: "Espada de Favonius", weaponType: "sword", stars: 4, img: "https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/weapons/redhorn_stonethresher.png?strip=all&quality=100&w=236"},
+
+// espadas 4 estrellas
+    { name: "Anemona Gemela", weaponType: "sword", stars: 4, img: "https://i2.wp.com/genshinbuilds.aipurrjects.com/genshin/weapons/redhorn_stonethresher.png?strip=all&quality=100&w=236"},
     { name: "Espada de Favonius", weaponType: "sword", stars: 4, img: "assets/weapons/favonius_sword.png"},
     { name: "Espada de Favonius", weaponType: "sword", stars: 4, img: "assets/weapons/favonius_sword.png" },
     { name: "Espada de Favonius", weaponType: "sword", stars: 4, img: "assets/weapons/favonius_sword.png" },
