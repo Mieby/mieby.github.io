10dfcfacabcfee7581405959207a4a6fd73dfd34
Mieby <emmashon2019@gmail.com>
2024-12-11 17:39:36 -0300

    Update translations.js

--- translations.js
+++ translations.js
@@ -1,1 +1,54 @@
-a
+const translations = {
+    es: {
+        title: "Gestión de Personajes de Genshin",
+        addCharacter: "Agregar Personaje",
+        toggleInfo: "Info+",
+        enableDrag: "Mover Personajes",
+        characters: {
+            Sayu: {
+                reloj: "Coming Soon",
+                caliz: "Coming Soon",
+                corona: "Coming Soon",
+                subs: "Coming Soon",
+                items: [
+                    { name: "Coming Soon", img: "ruta/a/imagen/Coming Soon.png" },
+                    { name: "Coming Soon", img: "ruta/a/imagen/Coming Soon.png" },
+                ],
+                weapons: [
+                    { name: "Coming Soon", img: "ruta/a/imagen/espada_de_fuego.png" },
+                    { name: "Coming Soon", img: "ruta/a/imagen/espada_legendaria.png" },
+                ],
+                artifacts: [
+                    { name: "Coming Soon", img: "ruta/a/imagen/artefacto1.png" },
+                    { name: "Coming Soon", img: "ruta/a/imagen/artefacto2.png" },
+                ],
+            },
+        },
+    },
+    en: {
+        title: "Genshin Character Management",
+        addCharacter: "Add Character",
+        toggleInfo: "Info+",
+        enableDrag: "Move Characters",
+        characters: {
+            Sayu: {
+                reloj: "Coming Soon",
+                caliz: "Coming Soon",
+                corona: "Coming Soon",
+                subs: "Coming Soon",
+                items: [
+                    { name: "Coming Soon", img: "ruta/a/imagen/Coming Soon.png" },
+                    { name: "Coming Soon", img: "ruta/a/imagen/Coming Soon.png" },
+                ],
+                weapons: [
+                    { name: "Coming Soon", img: "ruta/a/imagen/espada_de_fuego.png" },
+                    { name: "Coming Soon", img: "ruta/a/imagen/espada_legendaria.png" },
+                ],
+                artifacts: [
+                    { name: "Coming Soon", img: "ruta/a/imagen/artefacto1.png" },
+                    { name: "Coming Soon", img: "ruta/a/imagen/artefacto2.png" },
+                ],
+            },
+        },
+    },
+};
