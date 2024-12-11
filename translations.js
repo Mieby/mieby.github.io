35412a3b9a48db4e707eb413add79325d3dd911c
Mieby <emmashon2019@gmail.com>
2024-12-11 18:46:00 -0300

    Update translations.js

--- translations.js
+++ translations.js
@@ -2,10 +2,12 @@ const translations = {
     en: {
         addCharacter: "Add Character",
         moveCharacters: "Move Characters"
+        byeData: "Reset All Data"
     },
     es: {
         addCharacter: "Agregar Personaje",
         moveCharacters: "Mover Personajes"
+        byeData: "Borrar Todos los Datos"
     }
 };
 
@@ -19,6 +21,7 @@ function setLanguage(lang) {
 function updateUI() {
     document.getElementById('add-character-btn').innerText = translations[currentLang].addCharacter;
     document.getElementById('toggle-info-btn').innerText = translations[currentLang].moveCharacters;
+    document.getElementById('reset-data').innerText = translations[currentLang].resetData;
     // Agrega más elementos según sea necesario
 }
 
