dac494be41923fb620e3f0f279766f0752b7ec90
Mieby <emmashon2019@gmail.com>
2024-12-11 20:24:22 -0300

    Update genshinWeapons.js

--- genshinWeapons.js
+++ genshinWeapons.js
@@ -1,10 +1,10 @@
 const weaponsList = [
 
 // espadas 4 estrellas
-   { name: "Anemona Gemela", type: "sword", stars: 4, img: "assets/weapons/Amenoma Gemela.png"},
-   { name: "Espada del peñasco oscuro", type: "sword", stars: 4, img: "assets/weapons/Espada del peñasco oscuro.png"},
-   { name: "Espada de Favonius" , type: "bow", stars: 5, img: "assets/weapons/favonius_sword.png" },
-   { name: "Espada de Favonius", type: "catalist", stars: 5, img: "assets/weapons/favonius_sword.png" },
-   { name: "Espada de Favonius", type: "claymore", stars: 5, img: "assets/weapons/favonius_sword.png"}
+   const weaponsList = [
+    { name: { en: "Twin Nephrite", es: "Gemela Nephrite" }, type: "Espada", stars: 4, img: "assets/weapons/Amenoma Gemela.png" },
+    { name: { en: "Dark Cliff Sword", es: "Espada del Peñasco Oscuro" }, type: "Espada", stars: 4, img: "assets/weapons/Espada del peñasco oscuro.png" },
+    { name: { en: "Favonius Sword", es: "Espada de Favonius" }, type: "Espada", stars: 5, img: "assets/weapons/favonius_sword.png" },
+    // Agrega más armas aquí...
 ];
 
