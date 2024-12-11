d135020b890abec4ede962fc295da24d5a3f02f5
Mieby <emmashon2019@gmail.com>
2024-12-11 19:30:39 -0300

    Update genshinWeapons.js

--- genshinWeapons.js
+++ genshinWeapons.js
@@ -1,10 +1,11 @@
 const weaponsList = [
 
 // espadas 4 estrellas
-    { name: { es: "Anemona Gemela", en: "Twin Nephrite" } , type: "sword", stars: 4, img: "assets/weapons/Amenoma Gemela.png"},
-    { name: { es: "Espada del peñasco oscuro", en: "Darkrock Sword" } , type: "sword", stars: 4, img: "assets/weapons/Espada del peñasco oscuro.png"},
-    { name: { es: "Espada de Favonius", en: "Favonius Sword" } , type: "bow", stars: 5, img: "assets/weapons/favonius_sword.png" },
-    { name: { es: "Espada de Favonius", en: "Favonius Sword" }, type: "catalist", stars: 5, img: "assets/weapons/favonius_sword.png" },
-    { name: { es: "Espada de Favoniusa", en: "Favonius Sword" }, type: "claymore", stars: 5, img: "assets/weapons/favonius_sword.png"}
+   // { name: { es: "Anemona Gemela", en: "Twin Nephrite" } , type: "sword", stars: 4, img: "assets/weapons/Amenoma Gemela.png"},
+   // { name: { es: "Espada del peñasco oscuro", en: "Darkrock Sword" } , type: "sword", stars: 4, img: "assets/weapons/Espada del peñasco oscuro.png"},
+   // { name: { es: "Espada de Favonius", en: "Favonius Sword" } , type: "bow", stars: 5, img: "assets/weapons/favonius_sword.png" },
+    //{ name: { es: "Espada de Favonius", en: "Favonius Sword" }, type: "catalist", stars: 5, img: "assets/weapons/favonius_sword.png" },
+    //{ name: { es: "Espada de Favoniusa", en: "Favonius Sword" }, type: "claymore", stars: 5, img: "assets/weapons/favonius_sword.png"}
+    { name: Espada de Favonius, type: "claymore", stars: 5, img: "assets/weapons/favonius_sword.png"}
 ];
 
