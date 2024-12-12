f490e14a7dba5f0f70b4a7bb5ccc24375f323ec5
Mieby <emmashon2019@gmail.com>
2024-12-12 14:27:10 -0300

    Update genshinCharacters.js

--- genshinCharacters.js
+++ genshinCharacters.js
@@ -4,175 +4,174 @@ const genshinCharacters = [
     { name: "Sayu", element: "Anemo", stars: 4, weaponType: "claymore", img: "assets/characters/Sayu.png",
         reloj: "Coming Soon", caliz: "Coming Soon", corona: "Coming Soon", subs: "Coming Soon",
         items: [
-            { name: "Coming Soon", img: "ruta/a/imagen/Coming Soon.png" },
-            { name: "Coming Soon", img: "ruta/a/imagen/Coming Soon.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/Coming Soon.png" },
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/Coming Soon.png" }
         ],
         weapons: [
-            { name: "Coming Soon", img: "ruta/a/imagen/espada_de_fuego.png" },
-            { name: "Coming Soon", img: "ruta/a/imagen/espada_legendaria.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/espada_de_fuego.png" },
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/espada_legendaria.png" }
         ],
         artifacts: [
-            { name: "Coming Soon", img: "ruta/a/imagen/artefacto1.png" },
-            { name: "Coming Soon", img: "ruta/a/imagen/artefacto2.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/artefacto1.png" },
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/artefacto2.png" }
         ]
     },
     { name: "Lynette", element: "Anemo", stars: 4, weaponType: "sword", img: "assets/characters/Lynette.png",
              reloj: "Coming Soon", caliz: "Coming Soon", corona: "Coming Soon", subs: "Coming Soon",
         items: [
-            { name: "Coming Soon", img: "ruta/a/imagen/objeto1.png" },
-            { name: "Coming Soon", img: "ruta/a/imagen/objeto2.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/objeto1.png" },
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/objeto2.png" }
         ],
         weapons: [
             { name: "Espada de viento", img: "ruta/a/imagen/espada_de_viento.png" }
         ],
         artifacts: [
-            { name: "Coming Soon", img: "ruta/a/imagen/artefacto1.png" },
-            { name: "Coming Soon", img: "ruta/a/imagen/artefacto2.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/artefacto1.png" },
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/artefacto2.png" }
         ]
     },
     { name: "Faruzán", element: "Anemo", stars: 4, weaponType: "catalyst", img: "assets/characters/Faruzan-new.png",
               reloj: "Coming Soon", caliz: "Coming Soon", corona: "Coming Soon", subs: "Coming Soon",
         items: [
-            { name: "Coming Soon", img: "ruta/a/imagen/objeto1.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/objeto1.png" }
         ],
         weapons: [
-            { name: "Coming Soon", img: "ruta/a/imagen/caliz_de_viento.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/caliz_de_viento.png" }
         ],
         artifacts: [
-            { name: "Coming Soon", img: "ruta/a/imagen/artefacto1.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/artefacto1.png" }
         ]
     },
     { name: "Heizou", element: "Anemo", stars: 4, weaponType: "catalyst", img: "assets/characters/Heizou-new.png",
               reloj: "Coming Soon", caliz: "Coming Soon", corona: "Coming Soon", subs: "Coming Soon",
         items: [
-            { name: "Coming Soon", img: "ruta/a/imagen/objeto1.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/objeto1.png" }
         ],
         weapons: [
-            { name: "Coming Soon", img: "ruta/a/imagen/varita_del_viento.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/varita_del_viento.png" }
         ],
         artifacts: [
-            { name: "Coming Soon", img: "ruta/a/imagen/artefacto1.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/artefacto1.png" }
         ]
     },
     { name: "Sacarosa", element: "Anemo", stars: 4, weaponType: "catalyst", img: "assets/characters/Sacarosa.png",
               reloj: "Coming Soon", caliz: "Coming Soon", corona: "Coming Soon", subs: "Coming Soon",
         items: [
-            { name: "Margarita Voladora", img: "assets/Items/recolleccion/Margatita voladora.png" },
-            { name: "Néctar energético", img: "assets/Items/monstruitos/Nectar energetico.png" },
-            { name: "Semilla de Huracán", img: "assets/Items/minor boss/Semilla de huracan.png" }
+            { name: { en: "Margarita Voladora", es: "Margarita Voladora" }, img: "assets/Items/recolleccion/Margatita voladora.png" },
+            { name: { en:"Néctar energético", es: "Margarita Voladora" }, img: "assets/Items/monstruitos/Nectar energetico.png" },
+            { name: { en:"Semilla de Huracán", es: "Margarita Voladora" }, img: "assets/Items/minor boss/Semilla de huracan.png" }
         ],
         weapons: [
-            { name: "Filosofía de la Libertad", img: "assets/Items/Libritos/filosofia de la libertad.png" },
-            { name: "Alma de Boreas", img: "assets/Items/weekly boss/Alma de Boreas.png" },
+            { name: { en: "Filosofía de la Libertad", es: "Margarita Voladora" }, img: "assets/Items/Libritos/filosofia de la libertad.png" },
+            { name: { en: "Alma de Boreas", es: "Margarita Voladora" }, img: "assets/Items/weekly boss/Alma de Boreas.png" },
         ],
         artifacts: [
-            { name: " Coming soon", img: "ruta/a/imagen/artefacto1.png" }
+            { name:  { en: "Coming soon", es: "Margarita Voladora" }, img: "ruta/a/imagen/artefacto1.png" }
         ]
-    },
-                                                                                                                                                        // Anemo 5
+    },                                                                                                                                                     // Anemo 5
     { name: "Xiao", element: "Anemo", stars: 5, weaponType: "lance", img: "assets/characters/Anemo5/Xiao.png",
         reloj: "Coming Soon", caliz: "Coming Soon", corona: "Coming Soon", subs: "Coming Soon",
         items: [
-            { name: "Coming Soon", img: "ruta/a/imagen/Coming Soon.png" },
-            { name: "Coming Soon", img: "ruta/a/imagen/Coming Soon.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/Coming Soon.png" },
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/Coming Soon.png" }
         ],
         weapons: [
-            { name: "Coming Soon", img: "ruta/a/imagen/espada_de_fuego.png" },
-            { name: "Coming Soon", img: "ruta/a/imagen/espada_legendaria.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/espada_de_fuego.png" },
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/espada_legendaria.png" }
         ],
         artifacts: [
-            { name: "Coming Soon", img: "ruta/a/imagen/artefacto1.png" },
-            { name: "Coming Soon", img: "ruta/a/imagen/artefacto2.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/artefacto1.png" },
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/artefacto2.png" }
         ]
     },
     { name: "Jean", element: "Anemo", stars: 5, weaponType: "sword", img: "assets/characters/Anemo5/Jean.png",
         reloj: "Coming Soon", caliz: "Coming Soon", corona: "Coming Soon", subs: "Coming Soon",
         items: [
-            { name: "Coming Soon", img: "ruta/a/imagen/Coming Soon.png" },
-            { name: "Coming Soon", img: "ruta/a/imagen/Coming Soon.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/Coming Soon.png" },
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/Coming Soon.png" }
         ],
         weapons: [
-            { name: "Coming Soon", img: "ruta/a/imagen/espada_de_fuego.png" },
-            { name: "Coming Soon", img: "ruta/a/imagen/espada_legendaria.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/espada_de_fuego.png" },
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/espada_legendaria.png" }
         ],
         artifacts: [
-            { name: "Coming Soon", img: "ruta/a/imagen/artefacto1.png" },
-            { name: "Coming Soon", img: "ruta/a/imagen/artefacto2.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/artefacto1.png" },
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/artefacto2.png" }
         ]
     },
     { name: "Kazuha", element: "Anemo", stars: 5, weaponType: "sword", img: "assets/characters/Anemo5/Kazuha.png",
         reloj: "Coming Soon", caliz: "Coming Soon", corona: "Coming Soon", subs: "Coming Soon",
         items: [
-            { name: "Coming Soon", img: "ruta/a/imagen/Coming Soon.png" },
-            { name: "Coming Soon", img: "ruta/a/imagen/Coming Soon.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/Coming Soon.png" },
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/Coming Soon.png" }
         ],
         weapons: [
-            { name: "Coming Soon", img: "ruta/a/imagen/espada_de_fuego.png" },
-            { name: "Coming Soon", img: "ruta/a/imagen/espada_legendaria.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/espada_de_fuego.png" },
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/espada_legendaria.png" }
         ],
         artifacts: [
-            { name: "Coming Soon", img: "ruta/a/imagen/artefacto1.png" },
-            { name: "Coming Soon", img: "ruta/a/imagen/artefacto2.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/artefacto1.png" },
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/artefacto2.png" }
         ]
     },
     { name: "Venti", element: "Anemo", stars: 5, weaponType: "bow", img: "assets/characters/Anemo5/Venti.png",
         reloj: "Coming Soon", caliz: "Coming Soon", corona: "Coming Soon", subs: "Coming Soon",
         items: [
-            { name: "Coming Soon", img: "ruta/a/imagen/Coming Soon.png" },
-            { name: "Coming Soon", img: "ruta/a/imagen/Coming Soon.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/Coming Soon.png" },
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/Coming Soon.png" }
         ],
         weapons: [
-            { name: "Coming Soon", img: "ruta/a/imagen/espada_de_fuego.png" },
-            { name: "Coming Soon", img: "ruta/a/imagen/espada_legendaria.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/espada_de_fuego.png" },
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/espada_legendaria.png" }
         ],
         artifacts: [
-            { name: "Coming Soon", img: "ruta/a/imagen/artefacto1.png" },
-            { name: "Coming Soon", img: "ruta/a/imagen/artefacto2.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/artefacto1.png" },
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/artefacto2.png" }
         ]
     },
     { name: "Chasca", element: "Anemo", stars: 5, weaponType: "bow", img: "assets/characters/Anemo5/Chasca.png",
         reloj: "Coming Soon", caliz: "Coming Soon", corona: "Coming Soon", subs: "Coming Soon",
         items: [
-            { name: "Coming Soon", img: "ruta/a/imagen/Coming Soon.png" },
-            { name: "Coming Soon", img: "ruta/a/imagen/Coming Soon.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/Coming Soon.png" },
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/Coming Soon.png" }
         ],
         weapons: [
-            { name: "Coming Soon", img: "ruta/a/imagen/espada_de_fuego.png" },
-            { name: "Coming Soon", img: "ruta/a/imagen/espada_legendaria.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/espada_de_fuego.png" },
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/espada_legendaria.png" }
         ],
         artifacts: [
-            { name: "Coming Soon", img: "ruta/a/imagen/artefacto1.png" },
-            { name: "Coming Soon", img: "ruta/a/imagen/artefacto2.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/artefacto1.png" },
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/artefacto2.png" }
         ]
     },
     { name: "Scaramuccia", element: "Anemo", stars: 5, weaponType: "catalist", img: "assets/characters/Anemo5/Scara.png",
         reloj: "Coming Soon", caliz: "Coming Soon", corona: "Coming Soon", subs: "Coming Soon",
         items: [
-            { name: "Coming Soon", img: "ruta/a/imagen/Coming Soon.png" },
-            { name: "Coming Soon", img: "ruta/a/imagen/Coming Soon.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/Coming Soon.png" },
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/Coming Soon.png" }
         ],
         weapons: [
-            { name: "Coming Soon", img: "ruta/a/imagen/espada_de_fuego.png" },
-            { name: "Coming Soon", img: "ruta/a/imagen/espada_legendaria.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/espada_de_fuego.png" },
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/espada_legendaria.png" }
         ],
         artifacts: [
-            { name: "Coming Soon", img: "ruta/a/imagen/artefacto1.png" },
-            { name: "Coming Soon", img: "ruta/a/imagen/artefacto2.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/artefacto1.png" },
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/artefacto2.png" }
         ]
     },
     { name: "Xianyun", element: "Anemo", stars: 5, weaponType: "catalist", img: "assets/characters/Anemo5/Xianyun.png",
         reloj: "Coming Soon", caliz: "Coming Soon", corona: "Coming Soon", subs: "Coming Soon",
         items: [
-            { name: "Coming Soon", img: "ruta/a/imagen/Coming Soon.png" },
-            { name: "Coming Soon", img: "ruta/a/imagen/Coming Soon.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/Coming Soon.png" },
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/Coming Soon.png" }
         ],
         weapons: [
-            { name: "Coming Soon", img: "ruta/a/imagen/espada_de_fuego.png" },
-            { name: "Coming Soon", img: "ruta/a/imagen/espada_legendaria.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/espada_de_fuego.png" },
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/espada_legendaria.png" }
         ],
         artifacts: [
-            { name: "Coming Soon", img: "ruta/a/imagen/artefacto1.png" },
-            { name: "Coming Soon", img: "ruta/a/imagen/artefacto2.png" }
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/artefacto1.png" },
+            { name: { en: "Coming Soon", es: "Próximamente" }, img: "ruta/a/imagen/artefacto2.png" }
         ]
     },
    
