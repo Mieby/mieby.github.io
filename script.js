45eba5a10012fc5b712be3a742194755b8474332
Mieby <emmashon2019@gmail.com>
2024-12-20 15:10:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function resetData() {
     localStorage.clear();  // Borra todos los datos de LocalStorage
 }
 // Adjunta el evento al botón 'Borrar Datos'
-document.getElementById('reset-data').addEventListener('click', resetData);
+//document.getElementById('reset-data').addEventListener('click', resetData);
 
