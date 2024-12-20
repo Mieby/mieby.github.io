a0d961d97e0bc1e0fcd8e423dffff70ee4c5f29b
Mieby <emmashon2019@gmail.com>
2024-12-19 22:52:00 -0300

    Update genshin.js

--- genshin.js
+++ genshin.js
@@ -340,7 +340,7 @@ function addCharacterCard(character, isLoading = false) {
         <div class="character-objects">
     <div class="reloj"><strong>Reloj:</strong> ${character.reloj}</div>
     <div class="caliz"><strong>Caliz:</strong> ${character.caliz}</div>
-    <div class="corona"><strong>Corona:</strong> ${character.corona}</div>
+    <div class="corona"><strong>${currentLanguage === 'en' ? 'Circlet:' : 'Corona:'}</strong> ${character.corona}</div>
     <div class="subs"><strong>Subs:</strong> ${character.subs}</div>
         </div>
             <textarea class="editable-text">${character.additionalInfo || ''}</textarea>
@@ -584,7 +584,7 @@ function toggleCharacterInfo() {
                 <div class="character-objects">
         <div class="reloj"><strong>Reloj:</strong> ${character.reloj}</div>
         <div class="caliz"><strong>Caliz:</strong> ${character.caliz}</div>
-        <div class="corona"><strong>Corona:</strong> ${character.corona}</div>
+        <div class="corona"><strong>${currentLanguage === 'en' ? 'Circlet:' : 'Corona:'}</strong> ${character.corona}</div>
     </div>
                 <textarea class="editable-text">${card.querySelector("h4").textContent || ''}</textarea>
             `;
