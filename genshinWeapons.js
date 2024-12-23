fffa551a42ce68b7742d86ae2f65fe029e9aa94f
Mieby <emmashon2019@gmail.com>
2024-12-23 15:19:02 -0300

    Update genshinWeapons.js

--- genshinWeapons.js
+++ genshinWeapons.js
@@ -3,12 +3,12 @@ const weaponsList = [
 // espadas 4 estrellas
     { name: { en: "Amenoma Kageuchi", es: "Amenoma gemela" }, type: "sword", stars: 4, img: "assets/weapons/Amenoma Gemela.png" },
     { name: { en: "Blackcliff Longsword", es: "Espada del Peñasco Oscuro" }, type: "sword", stars: 4, img: "assets/weapons/Espada del peñasco oscuro.png" },
-    { name: { en: "Calamity of Eshu", es: "Calamidad de Eshu" }, type: "sword", stars: 5, img: "assets/weapons/espada 4/Calamidad de Eshu.png" },
-    { name: { en: "Cinnabar Spindle", es: "Huso de Cinabrio" }, type: "sword", stars: 5, img: "assets/weapons/espada 4/Huso de cinabrio.png" },
-    { name: { en: "Finale of the Deep", es: "Réquiem Abisal" }, type: "sword", stars: 5, img: "assets/weapons/espada 4/Requiem abisal.png" },
-    { name: { en: "Favonius Sword", es: "Espada de Favonius" }, type: "sword", stars: 5, img: "assets/weapons/espada 4/Espada de favonius.png" },
-    { name: { en: "Festering Desire", es: "Deseo Ponzoñoso" }, type: "sword", stars: 5, img: "assets/weapons/espada 4/Deseo ponzoñoso.png" },
-    { name: { en: "Fleuve Cendre Ferryman", es: "Vado del Río" }, type: "sword", stars: 5, img: "assets/weapons/espada 4/Vado del rio.png" },
+    { name: { en: "Calamity of Eshu", es: "Calamidad de Eshu" }, type: "sword", stars: 4, img: "assets/weapons/espada 4/Calamidad de Eshu.png" },
+    { name: { en: "Cinnabar Spindle", es: "Huso de Cinabrio" }, type: "sword", stars: 4, img: "assets/weapons/espada 4/Huso de cinabrio.png" },
+    { name: { en: "Finale of the Deep", es: "Réquiem Abisal" }, type: "sword", stars: 4, img: "assets/weapons/espada 4/Requiem abisal.png" },
+    { name: { en: "Favonius Sword", es: "Espada de Favonius" }, type: "sword", stars: 4, img: "assets/weapons/espada 4/Espada de favonius.png" },
+    { name: { en: "Festering Desire", es: "Deseo Ponzoñoso" }, type: "sword", stars: 4, img: "assets/weapons/espada 4/Deseo ponzoñoso.png" },
+    { name: { en: "Fleuve Cendre Ferryman", es: "Vado del Río" }, type: "sword", stars: 4, img: "assets/weapons/espada 4/Vado del rio.png" },
     // Agrega más armas aquí...
 ];
 
